5e6e28245ec2ae5752a3e6379d87152f2a064bdf
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-07-02 19:55:57 +0300

    new ReactJS

---
+++ Components_State/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    setTimeout(() => {
+        setCount(count + 1);
+    }, 1000);
+
+    return (
+        <>
+            <h2>Counter</h2>
+            <p>{count}</p>
+        </>
+    );
+}
