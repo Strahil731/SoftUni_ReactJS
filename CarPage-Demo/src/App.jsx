a70c158f29f90b81a3a9843c63172f51b067560d
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-07-03 15:08:09 +0300

    update

--- CarPage-Demo/src/App.jsx
+++ CarPage-Demo/src/App.jsx
@@ -2,6 +2,7 @@ import Navigation from "./components/Navigation";
 import FirstBody from "./components/FirstBody";
 import FeaturedCars from "./components/FeaturedCars";
 import PeopleEvalution from "./components/PeopleEvalution.jsx";
+import CarBrand from "./components/CarBrand.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
             <FirstBody />
             <FeaturedCars />
             <PeopleEvalution />
+            <CarBrand />
         </>
     )
 }
--- CarPage-Demo/src/components/PeopleEvalution.jsx
+++ CarPage-Demo/src/components/PeopleEvalution.jsx
@@ -1,20 +1,40 @@
 import "../styles/PeopleEvalution.css";
 import human1 from "../images/human1.jpg";
+import human2 from "../images/human2.jpg";
+import human3 from "../images/human3.jpg";
 
 export default function PeopleEvalution() {
     return (
         <div className="people_evalution">
             <h1>WHAT PEOPLE SAY</h1>
-            <hr/>
+            <hr />
             <div className="people_container">
                 <div className="current_human">
-                    <img src={human1} alt="human1"/>
+                    <img src={human1} alt="human1" />
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dolorem earum
                         exercitationem explicabo laborum nemo nihil nisi totam voluptate voluptatem! Consectetur
                         corporis labore molestias nam non omnis quae quisquam vero?
                     </p>
                     <h2>Galena Simeonova</h2>
+                </div>
+                <div className="current_human">
+                    <img src={human2} alt="human2" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dolorem earum
+                        exercitationem explicabo laborum nemo nihil nisi totam voluptate voluptatem! Consectetur
+                        corporis labore molestias nam non omnis quae quisquam vero?
+                    </p>
+                    <h2>Ivan Ivanov</h2>
+                </div>
+                <div className="current_human">
+                    <img src={human3} alt="human3" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dolorem earum
+                        exercitationem explicabo laborum nemo nihil nisi totam voluptate voluptatem! Consectetur
+                        corporis labore molestias nam non omnis quae quisquam vero?
+                    </p>
+                    <h2>Silverster Georgiev</h2>
                 </div>
             </div>
         </div>
