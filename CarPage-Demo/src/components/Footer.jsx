27321b01a3350db316ce6c2166948ea479a877e1
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-07-04 08:41:19 +0300

    Create Footer.jsx

---
+++ CarPage-Demo/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../styles/Footer.css";
+
+export default function Footer(){
+    return(
+        <div className="footer_container">
+            <h3>&copy; Strahil Dimitrov 2024</h3>
+        </div>
+    );
+}
