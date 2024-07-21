11aee60a084886f24464949926c444e7c6ba2e61
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-07-21 15:54:30 +0300

    update files

--- React-Routing/src/App.jsx
+++ React-Routing/src/App.jsx
@@ -1,11 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
 import Home from "./components/Home";
+import Pricing from "./components/Pricing";
+import About from './components/About';
+import PageNotFound from './components/ErrorPage';
 
 function App() {
 
     return (
-        <>
-            <Home />
-        </>
+        <div className="bg-white">
+            <Header />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </div>
     )
 }
 
